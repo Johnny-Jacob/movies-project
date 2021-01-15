647f4df2cbe62d2f67536a0fb47fe2ee4c9780cb
Jacob Gonzalez <jacob.e.gonzalez2@gmail.com>
2021-01-15 17:30:48 -0600

    Added movie info modal

--- js/main.js
+++ js/main.js
@@ -92,6 +92,89 @@ $(document).ready(() =>{
 
 	var query = new Query("https://comfortable-lace-snowboard.glitch.me/")
 
+	let genres = [{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},{"id":878,"name":"Science Fiction"},{"id":10770,"name":"TV Movie"},{"id":53,"name":"Thriller"},{"id":10752,"name":"War"},{"id":37,"name":"Western"}]
+
+
+	const getPosterImage = (name, callback) =>{
+
+        var input = ""
+        var args = String(name).split(" ")
+        args.forEach( (arg,i) =>{
+            if(i == args.length-1){
+                input += arg;
+            }else{
+                input += arg;
+                input += "%20"
+            } 
+        })
+
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEKEY}&language=en-US&query=${input}`;
+		const options = {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		};
+		fetch( url, options).then(
+			(res)=>{
+				callback(res)
+			}
+		)
+
+	}
+
+	const autoFillInfo = () =>{
+		var input = ""
+        var args = String($('#title').val()).split(" ")
+        args.forEach( (arg,i) =>{
+            if(i == args.length-1){
+                input += arg;
+            }else{
+                input += arg;
+                input += "%20"
+            } 
+        })
+
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIEKEY}&language=en-US&query=${input}`;
+		const options = {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		};
+		fetch( url, options).then(
+			(res)=>{
+				res.json().then(data=>{
+
+					let movie = data.results[0]
+
+					let title = $('#title').val(movie.original_title)
+					let genreString = ""
+					movie.genre_ids.forEach( (id,i) =>{ //filters though known genre id's and creates a string 
+						let filter = genres.filter((g)=>{
+							return g.id == id
+						});
+						if(i == movie.genre_ids.length-1){
+							genreString += `${filter[0].name}`
+						}else{
+							genreString += `${filter[0].name}, `
+						}
+						
+					})
+				
+					let genre = $('#genre').val(genreString)
+					let year = $('#year').val(movie.release_date)
+					let rating = $('#rating').val(Math.floor(movie.vote_average/2))
+					let director = $('#director').val()
+					let plot = $('#plot').val(movie.overview)
+					let actors = $('#actors').val()
+				})
+			}
+		)
+	}
+
+	
+
 	const addMovie = () => {
 		let title = $('#title').val()
 		let genre = $('#genre').val()
@@ -101,20 +184,30 @@ $(document).ready(() =>{
 		let plot = $('#plot').val()
 		let actors = $('#actors').val()
 
-		let newMovie = {
-			title,
-			rating,
-			year,
-			genre,
-			director,
-			plot,
-			actors
-		}
 
-		query.post(newMovie, (res)=>{
-			//confirm to user movie has been added
-			generateMoviePosters()
+		getPosterImage(title, (res)=>{
+			res.json().then( data=>{
+				
+				let newMovie = {
+					title,
+					rating,
+					year,
+					poster: `https://image.tmdb.org/t/p/w500/${data.results[0].poster_path}`,
+					genre,
+					director,
+					plot,
+					actors
+				}
+		
+				query.post(newMovie, (res)=>{
+					//confirm to user movie has been added
+					generateMoviePosters()
+				})
+			})
 		})
+	
+
+		
 
 	}
 
@@ -127,20 +220,28 @@ $(document).ready(() =>{
 		let plot = $('#plot').val()
 		let actors = $('#actors').val()
 
-		let editMovie = {
-			title,
-			rating,
-			year,
-			genre,
-			director,
-			plot,
-			actors
-		}
-
-		query.put(editMovie, id,(res)=>{
-			//confirm to user movie has been added
-			generateMoviePosters()
+		
+		getPosterImage(title, (res)=>{
+			res.json().then( data=>{
+				
+				let newMovie = {
+					title,
+					rating,
+					year,
+					poster: `img/placeholder.png`,
+					genre,
+					director,
+					plot,
+					actors
+				}
+		
+				query.put(editMovie, id,(res)=>{
+					//confirm to user movie has been added
+					generateMoviePosters()
+				})
+			})
 		})
+
 	}
 
 	const getMovie = (id = 1, callback = ()=>{
@@ -192,6 +293,26 @@ $(document).ready(() =>{
 
 	}
 
+	const modalMovieInfo = (id = 1) =>{
+
+		getMovie(id,(data)=>{ //we get the data and assign it to form
+			
+			console.log("test")
+			console.log(data)
+
+			$('#movieTitle').html(data.title)
+			$('#modalPoster').attr("src",data.poster);
+			$('#modalGenre').html(`<strong>Genre:</strong> ${data.genre}`)
+			$('#modalYear').html(`<strong>Year:</strong> ${data.year}`)
+			$('#modalRating').html(`<strong>Rating:</strong> ${data.rating}`)
+			$('#modalDirector').html(`<strong>Director:</strong> ${data.director}`)
+			$('#modalActors').html(`<strong>Actors:</strong> ${data.actors}`)
+			$('#modalPlot').html(`<strong>Plot:</strong> ${data.plot}`)
+
+		})
+
+	}
+
 	//Adds click function for menu
 	Array.from(document.getElementById("menu").children).forEach( (child, i)=>{
 		
@@ -210,9 +331,9 @@ $(document).ready(() =>{
 	})
 
 	let qType = 1
+	let currentId = 1
 
 	const typeSwitch = mode =>{
-		console.log(mode)
 		qType = mode;
 		if(mode === 0){ // 0 = Edit
 			$('#modeTitle').html('Edit Movie')
@@ -246,7 +367,6 @@ $(document).ready(() =>{
 		posters.appendChild(loading)
 		getMovies((data)=>{ //we get the data and assign it to form
 			posters.innerHTML = ""
-			console.log(data)
 			data.forEach( movie =>{
 				
 				//sets up the container
@@ -280,9 +400,15 @@ $(document).ready(() =>{
 
 				let infoButton = document.createElement("button")
 				infoButton.setAttribute("class","btn btn-primary position-relative")
+				infoButton.setAttribute("data-toggle","modal")
+				infoButton.setAttribute("data-target","#movieInfo")
 				infoButton.style.left = "-1.5em"
 				infoButton.style.top = "15em"
 				infoButton.innerHTML = "Info"
+				infoButton.onclick = (e)=>{
+					e.preventDefault();
+					modalMovieInfo(movie.id)
+				}
 				
 
 				let editButton = document.createElement("button")
@@ -293,6 +419,7 @@ $(document).ready(() =>{
 				editButton.onclick = (e)=>{
 					e.preventDefault();
 					typeSwitch(0)
+					currentId = movie.id
 					populateMovieForm(movie.id)
 				}
 
@@ -319,8 +446,13 @@ $(document).ready(() =>{
 			addMovie();
 			
 		} else if (qType === 0) {
-			editMovie();
+			editMovie(currentId);
 		}
+	});
+
+	$('#autoFillButton').click( (e) =>{
+		e.preventDefault();
+		autoFillInfo()
 	});
 
 
